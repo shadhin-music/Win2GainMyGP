7fc3275e52954663aa80b9a2c651720facb2d2ee
Md Abir Hossain <abir@cloud7bd.com>
2025-11-05 13:00:07 +0600

    Initial Commit

---
+++ Win_gpSDK_iOS_Test/AppDelegate.swift
@@ -0,0 +1,42 @@
+//
+//
+//  AppDelegate.swift
+//  Win_gpSDK_iOS_Test
+//
+//  Created by Md. Abir Hossain on 5/11/25.
+//  Contact me if anything is needed: 
+//                             Phone: +880 1521-717367
+//                             Email: mdabirhossain.dev@gmail.com
+//  ©️ 2025 Md. Abir Hossain. All rights reserved.
+//
+
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
