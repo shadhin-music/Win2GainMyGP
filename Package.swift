7f389ef5c29c0087e166370f68bbc967a2c5c892
Abdul Motalab Rakib <motalab@cloud7bd.com>
2026-03-03 11:21:20 +0600

    Package added for SPM

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "Win2GainMyGP",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "Win2GainMyGP",
+            targets: ["Win2GainMyGP"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Win2GainMyGP",
+            url: "https://github.com/shadhin-music/Win2GainMyGP/releases/download/1.1.3/Win2GainMyGP.xcframework.zip",
+            checksum: "6c5ecaa8ba9a1b661566bda45fb2b6d5bd2fba74f3e0037b7168408b4c3efe43"
+        )
+    ]
+)
