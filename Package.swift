6e4a595923e456fd1be1959d01082c5c5331db97
Abdul Motalab Rakib <motalab@cloud7bd.com>
2026-03-03 11:36:39 +0600

    Prepare release 1.1.4

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Win2GainMyGP",
-            url: "https://github.com/shadhin-music/Win2GainMyGP/releases/download/1.1.3/Win2GainMyGP.xcframework.zip",
+            url: "https://github.com/shadhin-music/Win2GainMyGP/releases/download/1.1.4/Win2GainMyGP.xcframework.zip",
             checksum: "6c5ecaa8ba9a1b661566bda45fb2b6d5bd2fba74f3e0037b7168408b4c3efe43"
         )
     ]
